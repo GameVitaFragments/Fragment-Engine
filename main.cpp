31dc02f28c06d13dc137e6079827474bc2fb23c0
HexPrism <bharathkrishnan33@gmail.com>
2023-09-12 07:56:14 +0530

    Integrated ECS + Logging

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ECS.h> 
+#include <Fraglog.hpp>
+
+int main()
+{
+    // std::cout << "Hello World" << std::endl;
+    
+    FragLog log;
+    log.write("Hello World Logging Exists");
+    return 0;
+}
